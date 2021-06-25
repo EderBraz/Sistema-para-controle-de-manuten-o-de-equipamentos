6c012a6278bf1024c345078a57b126a28786477c
ederbraz <eder_braz@hotmail.com>
2021-06-25 14:52:37 -0300

    refactor(dto): move dto package

--- src/main/java/com/projuris/testeprojuris/dto/OrdemServicoDTO.java
+++ src/main/java/com/projuris/testeprojuris/dto/OrdemServicoDTO.java
@@ -1,6 +1,9 @@
-package com.projuris.testeprojuris.model;
+package com.projuris.testeprojuris.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.projuris.testeprojuris.model.Cliente;
+import com.projuris.testeprojuris.model.Responsavel;
+import com.projuris.testeprojuris.model.StatusReparo;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
