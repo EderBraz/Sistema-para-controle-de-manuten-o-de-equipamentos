ef2d10b62510d8df1e240c5bb039d896b8d996d5
ederbraz <eder_braz@hotmail.com>
2021-06-24 22:30:20 -0300

    build: start api

---
+++ src/main/java/com/projuris/testeprojuris/TesteProjurisApplication.java
@@ -0,0 +1,13 @@
+package com.projuris.testeprojuris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteProjurisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteProjurisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projuris/testeprojuris/TesteProjurisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projuris.testeprojuris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteProjurisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
